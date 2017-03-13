5d038b57d879442d9ac02411f11d15796b4716c2
Jonathan Reeves <redhoodjt.jr@gmail.com>
2017-03-13 21:59:38 +0000

    Fixed files with bad comments and code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :select_plan, only: :new
+  
   
   # Extend default Devise gem behavoir so that
   # users signing up with the Pro account (plan ID 3) 
@@ -9,20 +9,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super do |resource|
       if params[:plan]
         resource.plan_id = params[:plan]
-        if resource.plan_id == 3
+        if resource.plan_id == 2
           resource.save_with_subscription
         else
           resource.save
         end
       end
-    end
-  end
-  
-  private
-  def select_plan
-    unless (params[:plan] == '1' || params[:plan] == '3')
-      flash[:notice] = "Please select a membership plan to sign up."
-      redirect_to root_url
     end
   end
 end
